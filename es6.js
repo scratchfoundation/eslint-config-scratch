37dc883b52a20f688cf26601f260aa83320827c7
Christopher Willis-Ford <cwillisf@media.mit.edu>
2020-03-18 22:07:47 -0700

    feat(es6): enable several new ES6 rules and update eslint dep
    BREAKING CHANGE: Enable several rules related to ES6 features, mostly
    promises and async/await but a few other features as well.
    
    One rule, 'require-atomic-updates', was introduced in ESLint 5.3.0 so
    that's now the minimum specified in peerDependencies.

--- es6.js
+++ es6.js
@@ -7,13 +7,23 @@ module.exports = {
             after: true
         }],
         'no-confusing-arrow': [2],
+        'no-duplicate-imports': [2],
+        'no-return-await': [2],
+        'no-template-curly-in-string': [2],
         'no-useless-computed-key': [2],
         'no-useless-constructor': [2],
+        'no-useless-rename': [2],
         'no-var': [2],
         'prefer-arrow-callback': [2],
         'prefer-const': [2, {destructuring: 'all'}],
+        'prefer-promise-reject-errors': [2],
+        'prefer-rest-params': [2],
+        'prefer-spread': [2],
         'prefer-template': [2],
+        'require-atomic-updates': [2],
+        'require-await': [2],
         'rest-spread-spacing': [2, 'never'],
+        'symbol-description': [2],
         'template-curly-spacing': [2, 'never']
     },
     env: {
